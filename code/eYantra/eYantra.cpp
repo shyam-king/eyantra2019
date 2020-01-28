fe3f609f9d4b617ce0bcb6894b389c3b358b0dc9
Jayamathangi <mathangi611@gmail.com>
2020-01-28 20:43:30 +0530

    ADC

--- code/eYantra/eYantra.cpp
+++ code/eYantra/eYantra.cpp
@@ -7,12 +7,13 @@
 #define F_CPU 14745600
 
 #include <avr/io.h>
-#include "MotorControl/MotorControl.h"
+
 #include "UART/UART.h"
 #include "helper.h"
-#include "Encoders/Encoders.h"
 #include <util/delay.h>
 #include<stdio.h>
+
+int val = 0;
 
 void enableGlobalInterrupts() {
 	SREG |= (1 << 7);
@@ -22,20 +23,71 @@ void disableGlobalInterrupts() {
 	SREG &= !(1 << 7);
 }
 
+
+float line_Sensor_Read(int pin)
+{
+	switch (pin)
+
+	{ 
+		case 0:  
+		ADMUX = 11000000; // Reading from ADC0
+		ADCSRA |= (1<< ADSC); // for starting a conversion
+		while(ADCSRA & (1<<ADIF) == 0);
+		val = ADC;
+		return val;
+		break;
+
+		case 1: ADMUX = 11000001; // Reading from ADC1
+		ADCSRA |= (1<< ADSC); // for starting a conversion
+		while(ADCSRA & (1<<ADIF) == 0);
+		val = ADC;
+		return val;
+		break;
+		
+		case 2:  ADMUX = 11000010;  // ADC2
+		ADCSRA |= (1<< ADSC); // for starting a conversion
+		while(ADCSRA & (1<<ADIF) == 0);
+		val = ADC;
+		return val;
+		break;
+	}
+}
+
+
+
 int main(void) {
 	enableGlobalInterrupts();
-	initEncoders();
 	Serial::begin();
-	
-	volatile unsigned int count = 0;
-	encoderCount0 = &count;
-	encoderCount1 = &count;
+	int result;
 	char temp[15];
 	
+	ADCSRA |= (1<<ADEN) | (1<<ADIE);
+	
+	Serial::println("the code starts here!");
+	
+	for (int i =0; i<10;i++) {
+		Serial::println("i");
+		_delay_ms(1000);
+	}
+	
+	Serial::println("bhf");
+	
     while(1) {
+		Serial::println("while");
+		result = line_Sensor_Read(0);
 		Serial::println("count:");
-		sprintf(temp, "%d", count);
+		sprintf(temp, "%d", result);
 		Serial::println(temp);
-		_delay_ms(100);
+		
+		result = line_Sensor_Read(1);
+		Serial::println("count:");
+		sprintf(temp, "%d", result);
+		Serial::println(temp);
+		
+		result = line_Sensor_Read(2);
+		Serial::println("count:");
+		sprintf(temp, "%d", result);
+		Serial::println(temp);
+		_delay_ms(1000);
     }
 }
