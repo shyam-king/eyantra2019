d568da8cbee35c5be8cbb4b4774dfbf50dae69e1
shyam-king <m.shyam.tnj@gmail.com>
2020-02-01 07:44:09 +0530

    Reset AS project

---
+++ code/eYantra/eYantra/eYantra.cpp
@@ -0,0 +1,17 @@
+/*
+ * eYantra.cpp
+ *
+ * Created: 2/1/2020 7:42:31 AM
+ *  Author: mshya
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
