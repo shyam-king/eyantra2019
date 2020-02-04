80377c91fa471194232bcd44f0510ae191d63e8e
shyam-king <m.shyam.tnj@gmail.com>
2020-02-04 23:36:30 +0530

    Add common config file

---
+++ code/eYantra/eYantra/configure.h
@@ -0,0 +1,25 @@
+/*
+ * configure.h
+ *
+ * Created: 2/4/2020 6:03:48 PM
+ *  Author: Shyam
+ */ 
+
+
+#ifndef CONFIGURE_H_
+#define CONFIGURE_H_
+
+#define LINE_SENSOR_THRESHOLD 500
+
+// MOTOR PINS
+#define MOTOR_R_FRONT 3
+#define MOTOR_R_BACK 2
+
+#define MOTOR_L_FRONT 1
+#define MOTOR_L_BACK 0
+
+#define MOTOR_L_SPEED OCR0B
+#define MOTOR_R_SPEED OCR0A
+
+
+#endif /* CONFIGURE_H_ */
