9a171bc5021c8cfc1344d531d40eb761f0e7f10c
shyam-king <m.shyam.tnj@gmail.com>
2020-02-05 14:49:07 +0530

    Interface Buzzer

---
+++ code/eYantra/eYantra/Buzzer/Buzzer.cpp
@@ -0,0 +1,22 @@
+/*
+ * Buzzer.cpp
+ *
+ * Created: 2/5/2020 2:36:56 PM
+ *  Author: Shyam
+ */ 
+
+#include "Buzzer.h"
+#include <avr/io.h>
+
+void Buzzer::init() {
+    DDRH |= (1 << 0);
+    PORTH &= ~(1 << 0);
+}
+
+void Buzzer::buzz() {
+    PORTH |= (1 << 0);
+}
+
+void Buzzer::stopBuzz() {
+    PORTH &= ~(1 << 0);
+}
---
+++ code/eYantra/eYantra/Buzzer/Buzzer.h
@@ -0,0 +1,21 @@
+/*
+ * Buzzer.h
+ *
+ * Created: 2/5/2020 2:36:46 PM
+ *  Author: Shyam
+ */ 
+
+
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+class Buzzer {
+    public:
+    static void init();
+    static void buzz();
+    static void stopBuzz();
+};
+
+
+
+#endif /* BUZZER_H_ */
